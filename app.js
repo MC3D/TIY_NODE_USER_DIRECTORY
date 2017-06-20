59b723795b4e4c8288b8f872297ab15ce588aaaf
MC3D <m.chapman.home@gmail.com>
2017-06-20 16:51:16 -0400

    adds user template

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
-const users = require('./data.js');
+const data = require('./data.js');
 
 // Register the mustache template engine
 app.engine('mustache', mustacheExpress());
@@ -12,10 +12,20 @@ app.set('view engine', 'mustache');
 // Tell express where are view files are located
 app.set('views', './views');
 
+app.use('/', express.static('public'));
+
+// static route
 app.get('/', function(req, res){
-  var context = users;
+  var context = data;
   res.render('index', context);
 });
+
+app.get('/:id', function(req, res) {
+  var context = data.users.filter(function(user){
+    return user.id == req.params.id;
+  })[0];
+  res.render('user', context);
+})
 
 app.listen(3000, function() {
   console.log('sucessfully started express application')
